d5fb87d582be75599e00ce2b11c2029417c56cad
Mackenzie Weaver <macknz7@gmail.com>
2019-06-04 08:38:29 -0300

    Create extract-state-logic-to-redux.js

---
+++ extract-state-logic-to-redux.js
@@ -0,0 +1,22 @@
+//https://learn.freecodecamp.org/front-end-libraries/react-and-redux/extract-state-logic-to-redux
+
+// define ADD, addMessage(), messageReducer(), and store here:
+const ADD = 'ADD';
+
+function addMessage (message) {
+  return ({
+    type: ADD,
+    message: message,
+  });
+}
+
+function messageReducer(state = [], action) {
+  switch(action.type){
+    case ADD:
+      return [...state, action.message];
+    default:
+      return state;
+  }
+}
+
+let store = Redux.createStore(messageReducer);
